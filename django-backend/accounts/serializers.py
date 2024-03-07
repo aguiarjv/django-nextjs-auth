bb89b734925eb048810bdd24ec9d49735de80dc2
aguiarjv <jvictoraguiar@live.com>
2024-03-07 20:46:19 -0300

    api is sending the expiration time of the tokens

--- django-backend/accounts/serializers.py
+++ django-backend/accounts/serializers.py
@@ -1,13 +1,18 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.conf import settings
 
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
 
+        access_lifetime = settings.SIMPLE_JWT.get('ACCESS_TOKEN_LIFETIME')
+        refresh_lifetime = settings.SIMPLE_JWT.get('REFRESH_TOKEN_LIFETIME')
+
         user = self.user
-        data["user_id"] = user.id
-        data["username"] = user.username
+
         data["email"] = user.email
+        data["access_expires"] = access_lifetime
+        data["refresh_expires"] = refresh_lifetime
 
         return data
