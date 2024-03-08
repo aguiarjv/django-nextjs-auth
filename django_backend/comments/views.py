6d9bced844e6b3b9eb47edd5c218c105b2aef1e0
aguiarjv <jvictoraguiar@live.com>
2024-03-08 12:30:16 -0300

    created a function to filter the comments based on the user

--- django_backend/comments/views.py
+++ django_backend/comments/views.py
@@ -4,16 +4,24 @@ from comments.models import Comment
 from comments.serializers import CommentSerializer
 
 
+def filtered_comments(user):
+    if (user.is_staff):
+        return Comment.objects.all()
+
+    return Comment.objects.filter(user=user)
+
+
 class CommentListCreateView(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        user = self.request.user
-        return Comment.objects.filter(user=user)
+        return filtered_comments(self.request.user)
 
 
 class CommentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return filtered_comments(self.request.user)
