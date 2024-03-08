3fa897fd4bc8ac2fb69174fd5d7c6369c51dcc22
aguiarjv <jvictoraguiar@live.com>
2024-03-07 21:16:54 -0300

    removed refresh token lifetime

--- django-backend/accounts/serializers.py
+++ django-backend/accounts/serializers.py
@@ -7,12 +7,10 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         data = super().validate(attrs)
 
         access_lifetime = settings.SIMPLE_JWT.get('ACCESS_TOKEN_LIFETIME')
-        refresh_lifetime = settings.SIMPLE_JWT.get('REFRESH_TOKEN_LIFETIME')
 
         user = self.user
 
         data["email"] = user.email
         data["access_expires_in"] = access_lifetime
-        data["refresh_expires_in"] = refresh_lifetime
 
         return data
