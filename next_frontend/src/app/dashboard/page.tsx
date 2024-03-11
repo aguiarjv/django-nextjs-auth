becf03936a1e3e86520ae4369c792ba1e0c2692b
aguiarjv <jvictoraguiar@live.com>
2024-03-11 09:01:30 -0300

    minor changes

--- next_frontend/src/app/dashboard/page.tsx
+++ next_frontend/src/app/dashboard/page.tsx
@@ -15,7 +15,7 @@ export default async function Dashboard({
   };
 }) {
   return (
-    <div className="p-6 flex flex-col gap-2 max-w-[1000px] mx-auto">
+    <div className="p-6 flex flex-col gap-2 max-w-[1000px] mx-auto pb-0">
       <DashHeader />
       <Suspense
         key={String(searchParams?.title) + String(searchParams?.page)}
