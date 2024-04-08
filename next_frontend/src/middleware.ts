20596e529f9c94756a948ebc6dd3df4945d6d072
aguiarjv <jvictoraguiar@live.com>
2024-04-08 17:33:55 -0300

    changed matcher to not match the root page

--- next_frontend/src/middleware.ts
+++ next_frontend/src/middleware.ts
@@ -15,5 +15,5 @@ export const config = {
    * - favicon.ico (favicon file)
    * - png files
    */
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$|$).*)"],
 };
